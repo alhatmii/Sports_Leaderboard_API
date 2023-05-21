77f6029ab2a0ed655fab740623650faa99023a63
alhatmii <alhatmii.150@gmail.com>
2023-05-21 14:58:52 +0400

    Wrote parameters for exsisting classes

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/Player.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/Player.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
@@ -13,8 +16,55 @@ import javax.persistence.Entity;
 
 public class Player {
 
-    String player_id;
-    String username;
-    String password;
-    String sport;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String sport;
+
+    public Player() {
+        // Default constructor required by JPA
+    }
+
+    public Player(String username, String password, String sport) {
+        this.username = username;
+        this.password = password;
+        this.sport = sport;
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
 }
--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/Team.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/Team.java
@@ -3,8 +3,11 @@ package com.CodeLine.Sports_Leaderboard_API.Models;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Getter
 @Setter
@@ -12,4 +15,47 @@ import javax.persistence.Entity;
 @Data
 
 public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String teamName;
+    private String sport;
+
+    public Team() {
+        // Default constructor required by JPA
+    }
+
+    public Team(String teamName, String sport) {
+        this.teamName = teamName;
+        this.sport = sport;
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
 }

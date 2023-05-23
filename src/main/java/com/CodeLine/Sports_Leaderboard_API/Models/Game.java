e1f6248cd448c988526c21f1c934095a19e0da59
alhatmii <alhatmii.150@gmail.com>
2023-05-23 10:25:19 +0400

    Replace Int with Integer

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/Game.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/Game.java
@@ -20,19 +20,19 @@ public class Game extends BaseEntity {
     private Long id;
 
     @ManyToOne
-    private Team team1;
+     Team team1;
 
     @ManyToOne
-    private Team team2;
+     Team team2;
 
-    private int score1;
-    private int score2;
+    Integer score1;
+    Integer score2;
 
     public Game() {
         // Default constructor required by JPA
     }
 
-    public Game(Team team1, Team team2, int score1, int score2) {
+    public Game(Team team1, Team team2, Integer score1, Integer score2) {
         this.team1 = team1;
         this.team2 = team2;
         this.score1 = score1;
@@ -41,51 +41,51 @@ public class Game extends BaseEntity {
 
     // Getters and Setters
 
-    public Long getId() {
-        return id;
-    }
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public Team getTeam1() {
+//        return team1;
+//    }
+//
+//    public void setTeam1(Team team1) {
+//        this.team1 = team1;
+//    }
+//
+//    public Team getTeam2() {
+//        return team2;
+//    }
+//
+//    public void setTeam2(Team team2) {
+//        this.team2 = team2;
+//    }
+//
+//    public int getScore1() {
+//        return score1;
+//    }
+//
+//    public void setScore1(int score1) {
+//        this.score1 = score1;
+//    }
+//
+//    public int getScore2() {
+//        return score2;
+//    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Team getTeam1() {
-        return team1;
-    }
-
-    public void setTeam1(Team team1) {
-        this.team1 = team1;
-    }
-
-    public Team getTeam2() {
-        return team2;
-    }
-
-    public void setTeam2(Team team2) {
-        this.team2 = team2;
-    }
-
-    public int getScore1() {
-        return score1;
-    }
-
-    public void setScore1(int score1) {
-        this.score1 = score1;
-    }
-
-    public int getScore2() {
-        return score2;
-    }
-
-    public void setScore2(int score2) {
+    public void setScore2(Integer score2) {
         this.score2 = score2;
     }
     @JoinColumn(name = "Team_id", referencedColumnName = "Match_id")
     @OneToOne(cascade = CascadeType.ALL)
     Team Team;
-
+    @OneToMany
     @JoinColumn(name = "Team_id", referencedColumnName = "Match_id")
-    @OneToMany(cascade = CascadeType.ALL)
+   //(cascade = CascadeType.ALL)
     List<Team> team;
 
 }

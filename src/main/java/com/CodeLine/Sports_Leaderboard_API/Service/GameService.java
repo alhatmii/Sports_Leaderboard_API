716898d7ceda5b5b77d0c5beca51161567de2689
alhatmii <alhatmii.150@gmail.com>
2023-05-24 15:37:15 +0400

    Adding to game Service

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Service/GameService.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Service/GameService.java
@@ -1,9 +1,26 @@
 package com.CodeLine.Sports_Leaderboard_API.Service;
 
+import com.CodeLine.Sports_Leaderboard_API.Models.Game;
+import com.CodeLine.Sports_Leaderboard_API.Repository.GameRepository;
+import com.CodeLine.Sports_Leaderboard_API.Repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameService {
-
+    @Autowired
+    TeamRepository teamRepository;
+    @Autowired
+    GameRepository gameRepository;
+    public void createGame(){
+        Game gameToCreate = new Game();
+//        gameToCreate.setgameInformation("This game can play with 2 teams");
+//        gameToCreate.setScoreTeam1(1);
+//        gameToCreate.setScoreTeam2(2);
+//        gameToCreate.setTeam1(teamRepository.getTeamById(1));
+//        gameToCreate.setTeam2(teamRepository.getTeamById(2));
+//        gameToCreate.setCreateDate(new Date());
+//        gameToCreate.setIsActive(true);
+//        gameRepository.save(gameToCreate);
 
 }

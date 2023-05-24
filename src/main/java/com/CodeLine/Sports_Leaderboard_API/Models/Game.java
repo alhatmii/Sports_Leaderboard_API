fb73f5ca75bb0ec6cb0d83c285ffa2cb4bb34a84
alhatmii <alhatmii.150@gmail.com>
2023-05-24 08:01:22 +0400

    Adding properties and the relations to the class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/Game.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/Game.java
@@ -14,78 +14,18 @@ import java.util.List;
 @Data
 
 public class Game extends BaseEntity {
-
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @ManyToOne
-//     Team team1;
-//
-//    @ManyToOne
-//     Team team2;
-//
-//    Integer score1;
-//    Integer score2;
-//
-//    public Game() {
-//        // Default constructor required by JPA
-//    }
-//
-//    public Game(Team team1, Team team2, Integer score1, Integer score2) {
-//        this.team1 = team1;
-//        this.team2 = team2;
-//        this.score1 = score1;
-//        this.score2 = score2;
-//    }
-//
-//    // Getters and Setters
-//
-////    public Long getId() {
-////        return id;
-////    }
-////
-////    public void setId(Long id) {
-////        this.id = id;
-////    }
-////
-////    public Team getTeam1() {
-////        return team1;
-////    }
-////
-////    public void setTeam1(Team team1) {
-////        this.team1 = team1;
-////    }
-////
-////    public Team getTeam2() {
-////        return team2;
-////    }
-////
-////    public void setTeam2(Team team2) {
-////        this.team2 = team2;
-////    }
-////
-////    public int getScore1() {
-////        return score1;
-////    }
-////
-////    public void setScore1(int score1) {
-////        this.score1 = score1;
-////    }
-////
-////    public int getScore2() {
-////        return score2;
-////    }
-//
-//    public void setScore2(Integer score2) {
-//        this.score2 = score2;
-//    }
-//    @JoinColumn(name = "Team_id", referencedColumnName = "Match_id")
-//    @OneToOne(cascade = CascadeType.ALL)
-//    Team Team;
-//    @OneToMany
-//    @JoinColumn(name = "Team_id", referencedColumnName = "Match_id")
-//   //(cascade = CascadeType.ALL)
-//    List<Team> team;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Integer gameId;
+    String gameInformation;
+    @OneToOne
+    @JoinColumn(name = "team1_id",referencedColumnName = "id")
+    Team team1;
+    @OneToOne
+    @JoinColumn(name = "team2_id",referencedColumnName = "id")
+    Team team2;
+    Integer scoreTeam1;
+    Integer scoreTeam2;
 
 }

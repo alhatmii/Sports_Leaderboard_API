9cb304c81d6fb9bc4d284e49e65b6b12df7dc4fb
alhatmii <alhatmii.150@gmail.com>
2023-05-24 15:42:42 +0400

    Adding in GameService

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Service/GameService.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Service/GameService.java
@@ -14,13 +14,13 @@ public class GameService {
     GameRepository gameRepository;
     public void createGame(){
         Game gameToCreate = new Game();
-//        gameToCreate.setgameInformation("This game can play with 2 teams");
-//        gameToCreate.setScoreTeam1(1);
-//        gameToCreate.setScoreTeam2(2);
-//        gameToCreate.setTeam1(teamRepository.getTeamById(1));
-//        gameToCreate.setTeam2(teamRepository.getTeamById(2));
-//        gameToCreate.setCreateDate(new Date());
-//        gameToCreate.setIsActive(true);
-//        gameRepository.save(gameToCreate);
+         gameToCreate.setgameInformation("This game can play with 2 teams");
+        gameToCreate.setScoreTeamA(1);
+        gameToCreate.setScoreTeamB(2);
+        gameToCreate.setTeamNumber1(teamRepository.getById(1));
+        gameToCreate.setTeamNumber2(teamRepository.getById(2));
+        gameToCreate.setCreateDate(new Date());
+        gameToCreate.setIsActive(true);
+        gameRepository.save(gameToCreate);
 
 }

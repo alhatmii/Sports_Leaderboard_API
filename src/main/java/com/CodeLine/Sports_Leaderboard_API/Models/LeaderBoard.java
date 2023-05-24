29e9fa7ede7858dee64b142f5282c4e731075d9d
alhatmii <alhatmii.150@gmail.com>
2023-05-24 08:13:36 +0400

    Adding new properities to leaderBoard class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/LeaderBoard.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/LeaderBoard.java
@@ -1,4 +1,25 @@
 package com.CodeLine.Sports_Leaderboard_API.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Setter
+@Getter
+@Entity
+@Data
 public class LeaderBoard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Integer leaderBoardId;
+    String teamName;
+    Integer wins;
+    Integer losses;
 }

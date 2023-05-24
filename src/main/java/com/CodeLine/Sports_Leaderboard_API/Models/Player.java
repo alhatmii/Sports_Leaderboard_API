4bf2fe116adcbc7cdbb073474ffc0165a43ae432
alhatmii <alhatmii.150@gmail.com>
2023-05-24 08:06:30 +0400

    adding new properites to player class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/Player.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/Player.java
@@ -12,68 +12,13 @@ import javax.persistence.*;
 @Data
 
 public class Player extends BaseEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String username;
-    private String password;
-    private String sport;
-
-    public Player() {
-        // Default constructor required by JPA
-    }
-
-    public Player(String username, String password, String sport) {
-        this.username = username;
-        this.password = password;
-        this.sport = sport;
-    }
-
-    // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getSport() {
-        return sport;
-    }
-
-    public void setSport(String sport) {
-        this.sport = sport;
-    }
+    @Column(name = "id")
+    Integer playerId;
+    String playerName;
 
     @ManyToOne
-    private Team team;
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-
+    @JoinColumn(name = "team_id",referencedColumnName = "id") 
+    Team team;
 }

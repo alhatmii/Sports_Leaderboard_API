5e1b043e04e7a8b6384d1d05b27b93c5be118d50
alhatmii <alhatmii.150@gmail.com>
2023-05-24 12:26:43 +0400

    interface PlayerRepository extends JpaRepository<Player, Integer>

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/LeaderBoardRepository.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/LeaderBoardRepository.java
@@ -1,7 +1,9 @@
 package com.CodeLine.Sports_Leaderboard_API.Repository;
 
+import com.CodeLine.Sports_Leaderboard_API.Models.LeaderBoard;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LeaderBoardRepository {
+public interface LeaderBoardRepository extends JpaRepository <LeaderBoard, Integer> {
 }
--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/PlayerRepository.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/PlayerRepository.java
@@ -1,7 +1,10 @@
 package com.CodeLine.Sports_Leaderboard_API.Repository;
 
+import com.CodeLine.Sports_Leaderboard_API.Models.LeaderBoard;
+import com.CodeLine.Sports_Leaderboard_API.Models.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlayerRepository {
+public interface PlayerRepository extends JpaRepository<Player, Integer> {
 }

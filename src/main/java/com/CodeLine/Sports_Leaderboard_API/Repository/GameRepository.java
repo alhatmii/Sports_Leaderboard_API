3a2bdad30c5418510da5f2f2ed9e8a9b069c171d
alhatmii <alhatmii.150@gmail.com>
2023-05-24 12:22:02 +0400

    interface GameRepository extends JpaRepository

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/GameRepository.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/GameRepository.java
@@ -1,7 +1,9 @@
 package com.CodeLine.Sports_Leaderboard_API.Repository;
 
+import com.CodeLine.Sports_Leaderboard_API.Models.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameRepository {
+public interface GameRepository extends JpaRepository <Game, Integer> {
 }

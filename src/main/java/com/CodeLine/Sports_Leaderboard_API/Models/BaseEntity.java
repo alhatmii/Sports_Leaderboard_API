ed0ab6667f14210920602c7f895341e0099bfff9
alhatmii <alhatmii.150@gmail.com>
2023-05-24 08:24:37 +0400

    Adding createDate and updateTimestamp notation to the BaseEntity

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/BaseEntity.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/BaseEntity.java
@@ -3,6 +3,8 @@ package com.CodeLine.Sports_Leaderboard_API.Models;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.MappedSuperclass;
 import java.util.Date;
@@ -15,9 +17,9 @@ import java.util.Date;
 public class BaseEntity {
 
 
-
+@CreatedDate
     Date createdDate;
-
+@UpdateTimestamp
     Date updatedDate;
 
     Boolean isActive;

105899c3d5c83fe919e8567fac5adfe5611f6773
alhatmii <alhatmii.150@gmail.com>
2023-05-24 16:00:48 +0400

    Adding in LeaderBoardController

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Controller/LeaderBoardController.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Controller/LeaderBoardController.java
@@ -1,4 +1,12 @@
 package com.CodeLine.Sports_Leaderboard_API.Controller;
 
+import lombok.val;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping (value = "LeaderBoard")
+
 public class LeaderBoardController {
+
 }

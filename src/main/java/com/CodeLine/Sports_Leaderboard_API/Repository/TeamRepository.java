7527736137202435b9875fd48814b1827f939d86
alhatmii <alhatmii.150@gmail.com>
2023-05-24 12:28:23 +0400

    interface TeamRepository extends JpaRepository<Team, Integer>

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/TeamRepository.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/TeamRepository.java
@@ -1,7 +1,9 @@
 package com.CodeLine.Sports_Leaderboard_API.Repository;
 
+import com.CodeLine.Sports_Leaderboard_API.Models.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TeamRepository {
+public interface TeamRepository extends JpaRepository<Team, Integer> {
 }

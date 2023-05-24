4da9564f9cf2761ceae02370ad8f6fcd354bea16
alhatmii <alhatmii.150@gmail.com>
2023-05-24 15:52:30 +0400

    Adding in GameController

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Controller/GameController.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Controller/GameController.java
@@ -1,4 +1,20 @@
 package com.CodeLine.Sports_Leaderboard_API.Controller;
 
+import com.CodeLine.Sports_Leaderboard_API.Service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "Game")
+
 public class GameController {
+    @Autowired
+    GameService gameService;
+
+    @RequestMapping(value = "createGame", method = RequestMethod.POST)
+    public void createGame() {
+        gameService.createGame();
+    }
 }

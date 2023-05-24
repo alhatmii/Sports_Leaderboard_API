4d2a19a0666009e6573297d5753d771271600d26
alhatmii <alhatmii.150@gmail.com>
2023-05-24 15:56:42 +0400

    Adding in TeamRepository

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/TeamRepository.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/TeamRepository.java
@@ -2,8 +2,12 @@ package com.CodeLine.Sports_Leaderboard_API.Repository;
 
 import com.CodeLine.Sports_Leaderboard_API.Models.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Integer> {
+    @Query(value = "select t from Team t where t.id=:teamId")
+    public Team getTeamById(@Param("teamId") Integer id);
 }

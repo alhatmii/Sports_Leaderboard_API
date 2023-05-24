c773c46a33dd5f13b9bfbc9f27d4d4ddb63d1442
alhatmii <alhatmii.150@gmail.com>
2023-05-24 16:02:18 +0400

    Adding in TeamController

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Controller/TeamController.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Controller/TeamController.java
@@ -1,4 +1,11 @@
 package com.CodeLine.Sports_Leaderboard_API.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "Team")
+
 public class TeamController {
+
 }

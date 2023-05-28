bd99195b704e863e25e73237091a03d114a4366a
alhatmii <alhatmii.150@gmail.com>
2023-05-28 07:35:23 +0400

    Adding registration class properites

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/Registration.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Models/Registration.java
@@ -1,4 +1,22 @@
 package com.CodeLine.Sports_Leaderboard_API.Models;
 
-public class Registration {
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+@Data
+public class Registration extends BaseEntity{
+    @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Integer registerId;
+    String userName;
+    String password;
+    String teamName;
+    String sportType;
 }

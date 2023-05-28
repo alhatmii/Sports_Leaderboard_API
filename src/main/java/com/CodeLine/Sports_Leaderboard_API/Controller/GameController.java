a62193164ebc6479e804a1ecdc26a21e4698eb66
alhatmii <alhatmii.150@gmail.com>
2023-05-28 07:31:20 +0400

    Adding new class of registration

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Controller/GameController.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Controller/GameController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "Game")
 
-public class GameController {
+public class
+GameController {
     @Autowired
     GameService gameService;
 

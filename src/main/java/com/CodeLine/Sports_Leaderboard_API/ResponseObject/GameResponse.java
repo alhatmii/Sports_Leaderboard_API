b9f81df120330fc0c2b4892f65ef7394bd685186
alhatmii <alhatmii.150@gmail.com>
2023-05-31 11:24:07 +0400

    Adding in Class GameResponse

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/GameResponse.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/GameResponse.java
@@ -1,4 +1,19 @@
 package com.CodeLine.Sports_Leaderboard_API.ResponseObject;
 
-public class GameResponse {
+import com.CodeLine.Sports_Leaderboard_API.RequestObject.TeamRequest;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class GameRequest {
+    Integer gameId;
+    String gameDescription;
+    TeamRequest teamRequest1;
+    TeamRequest teamRequest2;
+    Integer scoreTeam1;
+    Integer scoreTeam2;
+  
 }

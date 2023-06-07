30aa37c31b3628293372c773595b89dbeca61f86
alhatmii <alhatmii.150@gmail.com>
2023-06-07 07:46:47 +0400

    Adding propreties to game response class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/GameResponse.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/GameResponse.java
@@ -1,4 +1,19 @@
 package com.CodeLine.Sports_Leaderboard_API.ResponseObject;
 
+import com.CodeLine.Sports_Leaderboard_API.RequestObject.TeamRequest;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
 public class GameResponse {
+    Integer Id;
+    String Description;
+    TeamRequest tRequest1;
+    TeamRequest tRequest2;
+    Integer score1;
+    Integer score2;
+
 }

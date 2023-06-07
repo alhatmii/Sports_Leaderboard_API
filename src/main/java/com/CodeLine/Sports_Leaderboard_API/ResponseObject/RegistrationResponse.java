4c447021613e5f6352db9a63bfbb59002b21e1be
alhatmii <alhatmii.150@gmail.com>
2023-06-07 07:53:07 +0400

    Adding propreties to registration response class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/RegistrationResponse.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/RegistrationResponse.java
@@ -1,4 +1,16 @@
 package com.CodeLine.Sports_Leaderboard_API.ResponseObject;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
 public class RegistrationResponse {
+    Integer Id;
+    String user;
+    String password;
+    String tName;
+    String sportType;
 }

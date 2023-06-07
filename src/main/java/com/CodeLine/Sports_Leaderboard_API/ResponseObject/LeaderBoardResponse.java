b65a7920b174a08c08223ea1b05278b0bb752036
alhatmii <alhatmii.150@gmail.com>
2023-06-07 07:48:44 +0400

    Adding propreties to leaderboard response class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/LeaderBoardResponse.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/LeaderBoardResponse.java
@@ -1,4 +1,15 @@
 package com.CodeLine.Sports_Leaderboard_API.ResponseObject;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
 public class LeaderBoardResponse {
+    Integer Id;
+    String Name;
+    Integer wins;
+    Integer losses;
 }

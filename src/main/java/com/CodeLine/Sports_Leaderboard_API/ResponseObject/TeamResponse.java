d24e9b175af7e7fbffea208a848dacdb0b90039a
alhatmii <alhatmii.150@gmail.com>
2023-06-07 07:55:17 +0400

    Adding propreties to Team response class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/TeamResponse.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/TeamResponse.java
@@ -1,4 +1,16 @@
 package com.CodeLine.Sports_Leaderboard_API.ResponseObject;
 
+import com.CodeLine.Sports_Leaderboard_API.Models.Registration;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
 public class TeamResponse {
+    Integer Id;
+    Integer wins;
+    Integer losses;
+    Registration registration;
 }

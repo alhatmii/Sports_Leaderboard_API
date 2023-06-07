2b420fba07e829de8aec3981b638fb6a91a67a00
alhatmii <alhatmii.150@gmail.com>
2023-06-07 07:50:40 +0400

    Adding propreties to player response class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/PlayerResponse.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/PlayerResponse.java
@@ -1,4 +1,15 @@
 package com.CodeLine.Sports_Leaderboard_API.ResponseObject;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
 public class PlayerResponse {
+    Integer Id;
+    String Name;
+    String tName;
+    String sport;
 }

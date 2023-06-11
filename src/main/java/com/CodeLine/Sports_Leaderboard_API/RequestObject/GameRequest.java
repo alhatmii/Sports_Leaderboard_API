a14a0fc06c3fd40a51b779a02c649f33ff795775
alhatmii <alhatmii.150@gmail.com>
2023-06-11 12:41:53 +0400

    Remove the request from classes

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/RequestObject/GameRequest.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/RequestObject/GameRequest.java
@@ -19,27 +19,7 @@ public class GameRequest {
     Integer scoreTeamA;
     Integer scoreTeamB;
 
-    public static Game convertToGameRequest(GameRequest request) {
-        Game game = new Game();
-        game.setGameInformation(request.getGameInformation());
-        game.setTeamNumber1(TeamRequest.convertToTeamRequest(request.getTeamRequest1()));
-        game.setTeamNumber2(TeamRequest.convertToTeamRequest(request.getTeamRequest2()));
-        game.setScoreTeamA(request.getScoreTeamA());
-        game.setScoreTeamB(request.getScoreTeamB());
-        return game;
 
-    }
-
-    public static List<Game> convertListToGameRequest(List<GameRequest> gameRequestList) {
-        List<Game> gameList = new ArrayList<>();
-        if (!gameRequestList.isEmpty()) {
-            for (GameRequest gameRequest : gameRequestList) {
-                gameList.add(convertToGameRequest(gameRequest));
-            }
-        }
-        return gameList;
-
-    }
 }
 
 

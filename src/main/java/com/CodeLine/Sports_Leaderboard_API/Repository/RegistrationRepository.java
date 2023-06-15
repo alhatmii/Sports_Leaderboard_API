14252f14375144f70eca2a204be5a8bee6f1a1c3
alhatmii <alhatmii.150@gmail.com>
2023-06-15 09:35:03 +0400

    Commenting in RegistrationRepository Class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/RegistrationRepository.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/RegistrationRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+// To indicate that the Class is repository component
 @Repository
 public interface RegistrationRepository {
+
+    // To write the Query for SQL database
+    // To show Registration IDs by ID as parameter
     @Query(value = "select r from Registration r where r.id= :id")
     Registration getRegistrationId(@Param("id") Integer id);
 }

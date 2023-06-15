41c540818e167f08727ac9fac03ee95197c09483
alhatmii <alhatmii.150@gmail.com>
2023-06-15 09:32:42 +0400

    Commenting in LeaderBoardRepository Class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/LeaderBoardRepository.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/LeaderBoardRepository.java
@@ -8,11 +8,17 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+// To indicate that the Class is repository component
 @Repository
 public interface LeaderBoardRepository extends JpaRepository<LeaderBoard,Integer> {
+
+    // To write the Query for SQL database
+    // To show LeaderBoard by ID as parameter
     @Query(value = "select l from LeaderBoard l where l.id= :id")
     LeaderBoard getLeaderBoardById(@Param("id") Integer id);
 
+    // To write the Query for SQL database
+    // To show LeaderBoard By Wins
     @Query(value = "select l from LeaderBoard l order by l.wins")
     List<LeaderBoard> getLeaderBoardByWins();
 }

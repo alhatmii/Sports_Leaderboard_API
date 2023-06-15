62e2b0e97d0508ce51f042c6a218971436cd53eb
alhatmii <alhatmii.150@gmail.com>
2023-06-15 09:41:57 +0400

    Commenting in TeamResponse Class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/TeamResponse.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/TeamResponse.java
@@ -5,9 +5,13 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+// Makes it easier to work with your Java classes by generating helpful methods automatically (toString, equals, hashCode)
 @Data
+
+// To auto generate Getter & Setter for Properties
 @Getter
 @Setter
+
 public class TeamResponse {
     Integer Id;
     Integer wins;

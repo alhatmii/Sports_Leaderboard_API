8c6d18d5c3ff643b593a6e605c5831837bcad505
alhatmii <alhatmii.150@gmail.com>
2023-06-15 10:00:56 +0400

    Commenting in PlayerService Class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Service/PlayerService.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Service/PlayerService.java
@@ -10,14 +10,20 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.util.List;
 
+// Used to mark the Class as Service Component, To write the all methods required from the class to perform
+// responsible for processing and coordinating business logic
 @Service
 public class PlayerService {
 
+    // To Automatically Wire dependencies.
     @Autowired
     PlayerRepository playerRepository;
+
+    // To Automatically Wire dependencies.
     @Autowired
     TeamRepository teamRepository;
 
+    // Method to add players manually to Intellij
 //    public void RegisterPlayer() {
 //        Player player = new Player();
 //        player.setTeamName("ooo");
@@ -30,7 +36,9 @@ public class PlayerService {
 //
 //
 //    }
-public void RegisterPlayer(PlayerRequest playerRequest) {
+
+    // Method to add players manually from HTTP (Postman App)
+    public void RegisterPlayer(PlayerRequest playerRequest) {
     Player player = new Player();
     player.setTeamName(playerRequest.getTeamName());
     player.setPlayerName(playerRequest.getPlayerName());
@@ -39,19 +47,15 @@ public void RegisterPlayer(PlayerRequest playerRequest) {
     player.setUpdatedDate(new Date());
     player.setIsActive(Boolean.TRUE);
     playerRepository.save(player);
-
-
 }
 
-
+    // Method to show all Players that were stored in the Database
     public List<Player> getAllPlayers() {
-
         return playerRepository.getAllPlayer();
     }
 
-
+    // Method to Update particular Players that were stored in the Database
     public void updatePlayer() {
-
         Player player = new Player();
         player.setPlayerId(1);
         player.setTeamName("hwh1");
@@ -63,6 +67,7 @@ public void RegisterPlayer(PlayerRequest playerRequest) {
 
     }
 
+    // Method to show all Players by ID that were stored in the Database
     public Player getPlayerById(Integer id) {
         Player player = playerRepository.getPlayerId(id);
         return player;

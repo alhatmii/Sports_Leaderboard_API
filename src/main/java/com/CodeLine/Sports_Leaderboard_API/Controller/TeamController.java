fae89ee719e970b4cfe72b21c09f91a80999a9f7
alhatmii <alhatmii.150@gmail.com>
2023-06-15 09:27:01 +0400

    Commenting in TeamController Class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Controller/TeamController.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Controller/TeamController.java
@@ -6,17 +6,26 @@ import com.CodeLine.Sports_Leaderboard_API.Service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+// RESTful controller that handles incoming HTTP requests and produces the corresponding HTTP responses=
 @RestController
+
+// Map a method or a controller class to a specific URL path and HTTP method (Postman App)
 @RequestMapping(value = "Team")
 public class TeamController {
+
+    // To Automatically Wire dependencies.
     @Autowired
     TeamService teamService;
 
+    // We use it to show it postman app, depending in the way of method.
+    // This one used for showing Registeration of new Teams
     @RequestMapping(value = "RegisterNewTeam", method = RequestMethod.POST)
     public void registerNewTeam(@RequestBody TeamRequest teamRequest) {
         teamService.createTeam(teamRequest);
     }
 
+    // We use it to show it postman app, depending in the way of method.
+    // This one used for showing RegisterationID for the teams
     @RequestMapping( value = "getTeamById", method = RequestMethod.GET)
     public Team getTeamById(@RequestParam Integer id) {
         Team teamById = teamService.getTeamById(id);

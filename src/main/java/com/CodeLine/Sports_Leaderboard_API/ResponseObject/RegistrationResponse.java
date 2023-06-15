7bf709fba332ccde0a0017e8bc45ca91145cff1a
alhatmii <alhatmii.150@gmail.com>
2023-06-15 09:41:40 +0400

    Commenting in RegistrationResponse Class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/RegistrationResponse.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/ResponseObject/RegistrationResponse.java
@@ -4,9 +4,13 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+// Makes it easier to work with your Java classes by generating helpful methods automatically (toString, equals, hashCode)
 @Data
+
+// To auto generate Getter & Setter for Properties
 @Getter
 @Setter
+
 public class RegistrationResponse {
     Integer Id;
     String user;

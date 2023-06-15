b13ec2aa971dc81323c83a0c214574dab071abc4
alhatmii <alhatmii.150@gmail.com>
2023-06-15 09:33:59 +0400

    Commenting in PlayerRepository Class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/PlayerRepository.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/PlayerRepository.java
@@ -8,12 +8,17 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+// To indicate that the Class is repository component
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
 
+    // To write the Query for SQL database
+    // To show All Players
     @Query(value = "select s from Player s")
     List<Player> getAllPlayer();
 
+    // To write the Query for SQL database
+    // To show Player by ID as parameter
     @Query(value = "select m from Player m where m.id= :id")
     Player getPlayerId(@Param("id") Integer id);
 

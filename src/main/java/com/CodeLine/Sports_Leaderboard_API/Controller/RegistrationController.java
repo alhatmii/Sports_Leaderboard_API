a6f3d43ebe3ca185781c180b5fd346d8cfc628d5
alhatmii <alhatmii.150@gmail.com>
2023-06-15 09:25:15 +0400

    Commenting in RegisrtationController Class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Controller/RegistrationController.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Controller/RegistrationController.java
@@ -6,16 +6,26 @@ import com.CodeLine.Sports_Leaderboard_API.Service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+// RESTful controller that handles incoming HTTP requests and produces the corresponding HTTP responses
 @RestController
+
+// Map a method or a controller class to a specific URL path and HTTP method (Postman App)
 @RequestMapping (value = "Registration")
 public class RegistrationController {
+
+    // To Automatically Wire dependencies.
     @Autowired
     RegistrationService registrationService;
 
+    // We use it to show it postman app, depending in the way of method.
+    // This one used for showing RegisteredTeam
     @RequestMapping(value = "RegisterTeam", method = RequestMethod.POST)
     public void registerTeam(@RequestBody RegistrationRequest registrationRequest) {
         registrationService.RegisterTeam(registrationRequest);
     }
+
+    // We use it to show it postman app, depending in the way of method.
+    // This one used for showing Registeration by ID
     @RequestMapping( value = "getRegisterById", method = RequestMethod.GET)
     public Registration getRegistrationById(@RequestParam Integer id) {
         Registration registerById = registrationService.getRegistrationById(id);

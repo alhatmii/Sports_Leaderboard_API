1180e8463c467d2f8301f1f0f2f155897f2e9405
alhatmii <alhatmii.150@gmail.com>
2023-06-15 09:35:55 +0400

    Commenting in TeamRepository Class

--- src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/TeamRepository.java
+++ src/main/java/com/CodeLine/Sports_Leaderboard_API/Repository/TeamRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+// To indicate that the Class is repository component
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Integer> {
+
+    // To write the Query for SQL database
+    // To show Teams by ID as parameter
     @Query(value = "select t from Team t where t.id=:teamId")
     public Team getTeamById(@Param("teamId") Integer id);
 }
